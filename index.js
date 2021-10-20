cf058f8057bfddb5686291f67a4840550b53f9e2
yikai116 <1164323901@qq.com>
2021-10-20 17:37:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-var myMo = require('myModule/index.js');
+var myMo = require('myModule');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  myMo.test();
+  myMo();
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
